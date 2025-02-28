a1703b935f58f76208b47f102aadabbe63228fbf
Andreas Ahlgren <andreas.ahlgren@soprasteria.com>
2025-02-28 10:55:01 +0000

    added comments again

---
+++ comments.js
@@ -0,0 +1,8 @@
+//setup webserver
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var port = 3000;
+
+//setup database
